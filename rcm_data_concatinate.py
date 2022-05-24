8ae684ea6bfdb0c4f526b2b0c850b54a407888b6
heidar alavi <alavi.h@gmail.com>
2022-05-25 00:07:15 +0430

    update

--- rcm_data_concatinate.py
+++ rcm_data_concatinate.py
@@ -22,8 +22,30 @@ def asset_list(path):
     asset_list = np.empty(0)
     for f in list_of_files(path):
         asset_list=np.append(asset_list,sheets_name(f))
-        
     return np.unique(asset_list)
+
+
+def find_files(asset,path):
+    res=[]
+    for f in list_of_files(path):
+        sh_names=sheets_name(f)
+        if asset in sh_names:
+            res.append(f)
+            
+    return res
+    
+    
+def make_dataframe(asset,files):
+    col_names=['AssetNumber','TagNo','نام جز','نوع جز','سطح تجهیز','پارت نامبر PN','سریال نامبر SN','جز بالاتر (سریال نامبر بالاسری)','مشخصه فنی','توضیحات','نوع فنی']
+    res_df=pd.DataFrame(columns=col_names)
+    for f in files:
+        print(f,asset)
+        df=pd.read_excel(f,sheet_name=asset,header=0,names=col_names)
+        res_df=pd.concat([res_df,df])
+    
+    f_name='./results/{}.xlsx'.format(asset)
+    res_df.to_excel(f_name,sheet_name=asset,index=False)
+
 
 
 if __name__ == "__main__":
@@ -35,16 +57,10 @@ if __name__ == "__main__":
     
     ass_list = asset_list(mydirectories)
     
-    file_list=list_of_files(mydirectories)
+    #file_list=list_of_files(mydirectories)
     
-    for f in file_list:
-        names=sheets_name(f)
-        print(names)
-        if ass_list[0] in names:
-            df= pd.read_excel(f, asset_list[0])
-            print(df)
+    for asset in ass_list:
+        files=find_files(asset, mydirectories)
+        make_dataframe(asset, files)
         
-        
-    
-    
     
