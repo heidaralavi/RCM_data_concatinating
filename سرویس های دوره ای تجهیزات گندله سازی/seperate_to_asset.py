b036bd36318562a89e0abd5bbf73a328102e42f7
heidar alavi <alavi.h@gmail.com>
2022-11-17 12:16:02 +0330

    services grouped by asset category and periods

--- سرویس های دوره ای تجهیزات گندله سازی/seperate_to_asset.py
+++ سرویس های دوره ای تجهیزات گندله سازی/seperate_to_asset.py
@@ -3,14 +3,18 @@ import numpy as np
 
 
 df=pd.read_excel("service_list.xlsx")
+df['category-class'].fillna(0, inplace=True)
 
-gb = df.groupby('asset')    
+gb = df.groupby('category-class')    
 
 for item in gb.groups:
     print(item)
     print(gb.get_group(item))
-    file_name = ".\\test\{}.xlsx".format(item)
-    gb.get_group(item).to_excel(file_name,index=False)
+    sub_df=gb.get_group(item)
+    sub_gb=sub_df.groupby('priod')
+    for item2 in sub_gb.groups:
+        file_name = ".\\test\{}-{}.xlsx".format(item,item2)
+        sub_gb.get_group(item2).to_excel(file_name,index=False)
     print(file_name)
     
     
