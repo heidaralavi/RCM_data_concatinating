d99a5e7982515d8cffb23cbc625945e72d5bf137
heidar alavi <alavi.h@gmail.com>
2022-12-04 08:59:53 +0330

    updated

--- لیست تخصیص کارشناسان فنی و نظارت به تجهیزات/making_text_files/make_text_files.py
+++ لیست تخصیص کارشناسان فنی و نظارت به تجهیزات/making_text_files/make_text_files.py
@@ -9,8 +9,29 @@ import pandas as pd
 
 
 main_dict = pd.read_excel("Origin.xlsx", sheet_name=None)
-sheet_names=main_dict.keys()
+sheet_names=list(main_dict.keys())
+
+
 print(type(sheet_names))
+position_ID = main_dict[sheet_names[0]].to_dict(orient='records')
+system_ID = main_dict[sheet_names[1]].to_dict(orient='records')
+trade_ID = main_dict[sheet_names[2]].to_dict(orient='records')
+anzardaghigh = main_dict[sheet_names[3]].to_dict(orient='records')
+automasion = main_dict[sheet_names[4]].to_dict(orient='records')
+labratory = main_dict[sheet_names[5]].to_dict(orient='records')
+IT = main_dict[sheet_names[6]].to_dict(orient='records')
+transport = main_dict[sheet_names[7]].to_dict(orient='records')
+nasouz = main_dict[sheet_names[8]].to_dict(orient='records')
+
+
+
+
+ 
+#print(trade_ID)
+
+for items in nasouz:
+    print(items['نام کارشناس دفتر فنی'])
+    #line_text='{} {}\n'.format(items['Name'],items['ID'])
 
 '''
 for sheet in sheet_names:
